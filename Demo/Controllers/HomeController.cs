158d9394f59b51d069acbe01f65d803267bb3768
ZhennanHuang <zh4n15@soton.ac.uk>
2016-11-22 21:36:58 +0000

    try to connect mongodb but failed

--- Demo/Controllers/HomeController.cs
+++ Demo/Controllers/HomeController.cs
@@ -3,24 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Demo.Models;
+using MongoDB.Driver;
+using Demo.Properties;
 
 namespace Demo.Controllers
 {
     public class HomeController : Controller
     {
+        public MongoDatabase MongoDatabase;
+        public MongoCollection Collections;
         // GET: Home
+        public HomeController() {
+            var mongoClient = new MongoClient("mongodb://COMP6235:wkbbsdh8oDY2@svm-hf1g10-comp6235-temp.ecs.soton.ac.uk:27017/"+"health_data");
+            //var server = mongoClient.GetServer();
+            //MongoDatabase = server.GetDatabase(Settings.Default.DB);
+            //var db = mongoClient.GetDatabase(Settings.Default.DB);
+
+            MongoDatabase.Server.Ping();
+            
+        }
         public ActionResult Index()
         {
-
-            return View();
+            Collections = MongoDatabase.GetCollection("uk");
+            long count = Collections.Count();
+            return null;
         }
         
         [HttpPost]
-        public ActionResult charts(HttpContext context) {
-            context.Request.ContentType = "application/json";
-            string list = "sada:weq";
-            return Json(list);
+        public ActionResult charts(DropDownList ddl) {
+            if (ddl != null)
+                return Json("Success");
+            else
+                return Json("Error");
         }
-
     }
 }
---
+++ Demo/Models/DropDownList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demo.Models
+{
+    public class DropDownList
+    {
+        
+        public string t1 { get; set; }
+        public string t2 { get; set; }
+        public string t3 { get; set; }
+    }
+}
