9f6c8c979d115354efeb3116d358696dcb0ff705
ZhennanHuang <zh4n15@soton.ac.uk>
2016-11-23 18:47:41 +0000

    s

--- Demo/Controllers/HomeController.cs
+++ Demo/Controllers/HomeController.cs
@@ -16,11 +16,13 @@ namespace Demo.Controllers
         // GET: Home
         public HomeController() {
             var mongoClient = new MongoClient("mongodb://COMP6235:wkbbsdh8oDY2@svm-hf1g10-comp6235-temp.ecs.soton.ac.uk:27017/"+"health_data");
+
             //var server = mongoClient.GetServer();
             //MongoDatabase = server.GetDatabase(Settings.Default.DB);
             //var db = mongoClient.GetDatabase(Settings.Default.DB);
 
             mongoClient.GetServer().Ping();
+     
             
         }
         public ActionResult Index()
